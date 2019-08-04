404ee7d74081e5b9245d96174bb8877221b37f98
Thiago Saife <thiago@farofastudios.com.br>
2019-08-04 11:50:03 -0300

    Novos campos da listagem de transações

--- src/components/transactions/ListTransactions.vue
+++ src/components/transactions/ListTransactions.vue
@@ -1,24 +1,25 @@
 <template>
-  <div class="container mb-3">
+  <div class="mb-3">
     <table class="table">
       <thead>
         <tr>
           <th scope="col" v-for="(heading, index) in headings" :key="index">
-            {{ heading }}
+            <p class="small font-weight-bold">{{ heading }}</p>
           </th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="(transaction, index) in getTransactionsList"  :key="index">
+        <tr class="small" v-for="(transaction, index) in getTransactionsList"  :key="index">
           <td>{{ transaction.paymentDate }}</td>
-          <td>{{ transaction.status }}</td>
+          <td>{{ formatStatus(transaction.status) }}</td>
           <td>{{ transaction.price }}</td>
+          <td>{{ `${formatCurrency(transaction.fee)} (${transaction.feeApplied}%)` }}</td>
           <td>{{ transaction.description }}</td>
-          <td>{{ transaction.selectedMethod }}</td>
-          <td>{{ transaction.cardNumber }}</td>
+          <td>{{ formatMethod(transaction.selectedMethod) }}</td>
+          <td>{{ `**** **** **** ${transaction.cardNumber}` }}</td>
           <td>{{ transaction.cardOwner }}</td>
           <td>{{ transaction.cardValidityDate }}</td>
-          <td>{{ transaction.cardVerificationCode }}</td>
+          <td>{{ transaction.cardVerificationCode || '-' }}</td>
         </tr>
       </tbody>
     </table>
@@ -57,6 +58,7 @@ export default {
         'Data do pagamento',
         'Status',
         'Valor da transação',
+        'Valor com a taxa',
         'Descrição da transação',
         'Método de pagamento',
         'Número do cartão (4 últimos dígitos)',
@@ -64,12 +66,32 @@ export default {
         'Data de validade do cartão',
         'Código de verificação do cartão (CVV)',
       ],
+      methods: [
+        'credit_card',
+        'debit_card'
+      ],
       noData: {
         message: 'Não existem transações cadastradas',
         show: false,
       },
+      status: [
+        'paid',
+        'waiting_funds'
+      ],
       transactionsList: [],
     };
   },
+  methods: {
+    formatCurrency(value) {
+      const currency = value.toFixed(2);
+      return currency.replace('.', ',');
+    },
+    formatMethod(payMethod) {
+      return payMethod === this.methods[0] ? 'Crédito' : 'Débito';
+    },
+    formatStatus(payMethod) {
+      return payMethod === this.status[0] ? 'Pago' : 'Aguardando pagamento';
+    },
+  }
 }
 </script>
